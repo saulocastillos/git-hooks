744797236448cd794929ec4c5dfdbe20367ab96a
Saulo Silva Castilhos <altfreedom@gmail.com>
2019-10-15 19:23:32 -0300

    fix: updates

---
+++ build/precache-manifest.3a360b2547ad9f245f59d4c74851f3ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c54ba610fb1065b2a0a042c9022a7e",
+    "url": "/git-hooks/index.html"
+  },
+  {
+    "revision": "0223c54a2b2e7466252c",
+    "url": "/git-hooks/static/js/2.17d0ba90.chunk.js"
+  },
+  {
+    "revision": "4735ea0e8b8ade8901c8",
+    "url": "/git-hooks/static/js/main.2f629db3.chunk.js"
+  },
+  {
+    "revision": "b6b9eadff5a3e2cf06f4",
+    "url": "/git-hooks/static/js/runtime-main.b19abbe9.js"
+  }
+]);
