62d276a1648f61942531c84a522dcc36cb2a0f73
Saulo Silva Castillos <altfreedom@gmail.com>
2019-12-27 16:42:41 -0300

    new build

---
+++ build/precache-manifest.2368d8dfc5bd2341187f53be664203db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23f38af8019c43e1fad4c31ec77e3dd",
+    "url": "/git-hooks/index.html"
+  },
+  {
+    "revision": "78f8458daa6712246ff0",
+    "url": "/git-hooks/static/js/2.0993fe64.chunk.js"
+  },
+  {
+    "revision": "199a70e95de4d0cb2d73",
+    "url": "/git-hooks/static/js/main.ede3e2b1.chunk.js"
+  },
+  {
+    "revision": "dfcaaf2ec8920f7dcc54",
+    "url": "/git-hooks/static/js/runtime-main.b7580133.js"
+  }
+]);
